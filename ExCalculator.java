430415ad06a2d8fd3199ec12e5d962de6ae78e6d
Shawn Boutte <sbboo22@gmail.com>
2022-02-11 21:25:47 -0600

    updaed calc

--- ExCalculator.java
+++ ExCalculator.java
@@ -1,45 +1,82 @@
 package com.ravature.HW1Calculator;
 
+import java.beans.BeanProperty;
+import java.nio.channels.ScatteringByteChannel;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*I wrote this code to include as many OOPs as possible and I apologize in advance for over complication*/
-
 interface calculations{
     int Add(int i, int j);
     int Subtract(int i, int j);
     int Multiply(int i, int j);
     int Divide(int i, int j);
+    default void welcome(){
+        System.out.println("hello");
+    }
 }//opertions interfaces
 
 class getnumbers{
     protected int firstExponent;
     protected int secondExponent;
+    private boolean goodInput = false;
     boolean ran = false;
-    Scanner Inputs = new Scanner(System.in);
+        Scanner Inputs = new Scanner(System.in);
     public getnumbers(){
         getNumbers();
     }
     public void getNumbers(){
         System.out.print("Please enter Numbers to calculate" + "\nFirst Number: ");
-        firstExponent = Inputs.nextInt();
-        System.out.print("\nSecond number: ");
-        secondExponent = Inputs.nextInt();
+        while(!goodInput) {
+            try {
+                firstExponent = Inputs.nextInt();
+                goodInput = true;
+            } catch (InputMismatchException e) {
+                System.out.print("you must enter a number please try again: ");
+                Inputs.nextLine();
+                goodInput = false;
+            }
+        }
+        goodInput = false;
+        System.out.print("Second number: ");
+        while(!goodInput) {
+            try {
+                secondExponent = Inputs.nextInt();
+                goodInput = true;
+            } catch (InputMismatchException e) {
+                System.out.print("That was not a number please try again: ");
+                Inputs.nextLine();
+                goodInput = false;
+            }
+        }
+        goodInput = false;
     }
 }//includes funciton for getting numbers
 class calculate extends getnumbers implements calculations{
     private int solution;
     private int operation;
+    private boolean goodOperation = false;
     Scanner calculation = new Scanner(System.in);
     public calculate(){
         operation = 10;
         while (operation > 5 || operation < 5) {
-            if (ran) {
+            if (ran)
                 getNumbers();
-            }
             else
                 ran = true;
+
             System.out.println("Please enter the Operation you wold like to perform: \n1(Additon)\t2(Subtraction)\t3(Multiplication)\t4(Division)\t5(Quit)");
-            operation =calculation.nextInt();
+            while (!goodOperation) {
+                try {
+                    operation = calculation.nextInt();
+                    goodOperation = true;
+                } catch (InputMismatchException e) {
+                    System.out.print("That is not a vaild operation please try again: ");
+                    calculation.nextLine();
+                    goodOperation = false;
+                }
+            }
+            goodOperation = false;
             switch (operation) {
                 case 1:
                     System.out.println("Answer : " + Add(super.firstExponent, super.secondExponent));
@@ -116,11 +153,9 @@ class calculate extends getnumbers implements calculations{
         return solution;
     }
 }
-
 public class ExCalculator {
     public static void main(String[] args) {
         //calculate calculateWithout = new calculate(3,4,3);
         calculate calculateWithPrompt = new calculate();
     }
-
 }
